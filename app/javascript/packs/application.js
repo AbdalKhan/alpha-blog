85a6e3695986e154bd01af47690b6a910254a23b
Abdal Khan <abdal_ijaz_k@hotmail.com>
2019-11-17 19:26:03 -0700

    bootstrap nav and jumbotron

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,11 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+
+
 import "bootstrap";
 import "../stylesheets/application"
+
 
 document.addEventListener("turbolinks:load", () => {
 
