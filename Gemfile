6a3bc0043e1b9ebc89d452c8184861fe9fee3da1
Abdal Khan <abdal_ijaz_k@hotmail.com>
2019-11-19 15:47:46 -0700

    add pagination to views

--- Gemfile
+++ Gemfile
@@ -4,7 +4,8 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 ruby '2.6.0'
 
 
-gem 'bcrypt', '~> 3.1.7' 
+
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 6.0.1'
 # Use sqlite3 as the database for Active Record
@@ -25,7 +26,10 @@ gem 'jbuilder', '~> 2.7'
 # gem 'redis', '~> 4.0'
 # Use Active Model has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
+gem 'bcrypt', '~> 3.1.7' 
+gem 'will_paginate', '~> 3.2', '>= 3.2.1'
 
+gem 'will_paginate-bootstrap4'
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
 
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,9 @@
 class ArticlesController < ApplicationController
 
+	before_action :set_article, only: [:edit, :update, :show, :destroy ]
 
 	def index
-		@articles = Article.all
+		@articles = Article.paginate(page: params[:page], per_page: 5)
 	end
 
 	def new
@@ -23,16 +24,16 @@ class ArticlesController < ApplicationController
 	end
 
 	def show
-		@article = Article.find(params[:id])
+		
 	end
 
 	def edit
-		@article = Article.find(params[:id])
+		
 		@article.user = User.first  
 	end
 
 	def update
-		@article = Article.find(params[:id])
+		
 		@article.user = User.first
 		if @article.update(article_params)
 			flash[:success] = "Article was successfully updated"
@@ -43,16 +44,22 @@ class ArticlesController < ApplicationController
 	end
 
 	def destroy
-		@article = Article.find(params[:id])
+		
 		@article.destroy
 		flash[:danger] = "Article was successfully deleted"
 		redirect_to articles_path
 
 	end
 	
+	def set_article
+		@article = Article.find(params[:id])
+	end
+
 private
 	
 	def article_params
 		params.require(:article).permit(:title, :description)
 	end
+
+	
 end
